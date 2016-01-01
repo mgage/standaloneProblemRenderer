982d6a817befda547e3d13b28cec4d3e727b1566
Michael Gage <gage@math.rochester.edu>
2015-12-31 22:42:57 -0500

    Add small script that will redirect a text file in Bbedit through the standalone client.
    This is a sample -- something similar, but definitely not identical, can be used to connect other editors to the client.

---
+++ standalone_bbedit.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl -w
+die "BB_DOC_PATH is not present.  You must first save the document\n" unless defined $ENV{BB_DOC_PATH};
+$command = "/Volumes/WW_test/opt/local/bin/perl".
+" /Volumes/WW_test/opt/webwork/standaloneProblemRenderer/standalonePGproblemRenderer.pl ".
+" -bB $ENV{BB_DOC_PATH}";
+system($command);
